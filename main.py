0b1b991311b121cc5115eb4118a180b3519f6175
jmuriki <106560016+jmuriki@users.noreply.github.com>
2023-01-24 22:40:59 +0530

    Use long polling API

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import json
 import requests
 
 from dotenv import load_dotenv
@@ -7,12 +8,33 @@ from dotenv import load_dotenv
 def main():
 	load_dotenv()
 	token = os.environ["DEVMAN_TOKEN"]
-	url = "https://dvmn.org/api/user_reviews/"
+	long_polling_url = "https://dvmn.org/api/long_polling/"
 	header = {
 		"Authorization": f"Token {token}",
 	}
-	response = requests.get(url, headers=header)
-	response.raise_for_status()
+	timestamp = None
+	while True:
+		params = {
+			"timestamp": timestamp,
+		}
+		response = requests.get(long_polling_url, headers=header, params=params)
+		response.raise_for_status()
+		print(response.text)
+		response_dict = json.loads(response.text)
+		if response_dict.get("status") == "timeout" and not response_dict.get("request_query"):
+			timestamp = response_dict.get("timestamp_to_request")
+			print("FIRST WAITING", timestamp)
+		elif response_dict.get("status") == "timeout" and response_dict.get("request_query"):
+			timestamp = response_dict.get("timestamp_to_request")
+			print("AGAIN WAITING", timestamp)
+		elif response_dict.get("status") == "found" and not response_dict.get("request_query"):
+			timestamp = response_dict.get("last_attempt_timestamp")
+			print("GOT IT FAST", timestamp)
+		elif response_dict.get("status") == "found" and response_dict.get("request_query"):
+			timestamp = response_dict.get("last_attempt_timestamp")
+			print("GOT IT", timestamp)
+		else:
+			print("!!!!!!!!!!!!!!!!!!!!!!")
 
 
 if __name__ == "__main__":
