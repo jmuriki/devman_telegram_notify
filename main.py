d0af862c8c66fd56bf45ee2b23fb2b11f6055f85
jmuriki <106560016+jmuriki@users.noreply.github.com>
2023-01-25 15:10:21 +0530

    Add telegram notifications

--- main.py
+++ main.py
@@ -2,16 +2,26 @@ import os
 import json
 import time
 import requests
+import telegram
 
 from dotenv import load_dotenv
 
 
+def send_message(telegram_token, chat_id, message):
+	bot = telegram.Bot(token=telegram_token)
+	bot.send_message(
+        chat_id=chat_id,
+        text=message,
+    )
+
 def main():
 	load_dotenv()
-	token = os.environ["DEVMAN_TOKEN"]
+	devman_token = os.environ["DEVMAN_TOKEN"]
+	telegram_token = os.environ["TELEGRAM_TOKEN"]
+	chat_id = os.environ["TELEGRAM_CHAT_ID"]
 	long_polling_url = "https://dvmn.org/api/long_polling/"
 	header = {
-		"Authorization": f"Token {token}",
+		"Authorization": f"Token {devman_token}",
 	}
 	timestamp = None
 	while True:
@@ -21,28 +31,44 @@ def main():
 		try:
 			response = requests.get(long_polling_url, headers=header, params=params)
 			response.raise_for_status()
-			print(response.text)
+			message = "RESPONSE"
+			print(message, response.text)
+			send_message(telegram_token, chat_id, message)
 			response_dict = json.loads(response.text)
 			if response_dict.get("status") == "timeout" and not response_dict.get("request_query"):
 				timestamp = response_dict.get("timestamp_to_request")
-				print("WAITING FOR THE FIRST TIME", timestamp)
+				message = "WAITING FOR THE FIRST TIME"
+				print(message, timestamp)
+				send_message(telegram_token, chat_id, message)
 			elif response_dict.get("status") == "timeout" and response_dict.get("request_query"):
 				timestamp = response_dict.get("timestamp_to_request")
-				print("WAITING", timestamp)
+				message = "WAITING"
+				print(message, timestamp)
+				send_message(telegram_token, chat_id, message)
 			elif response_dict.get("status") == "found" and not response_dict.get("request_query"):
 				timestamp = response_dict.get("last_attempt_timestamp")
-				print("GOT IT FAST", timestamp)
+				message = "GOT IT FAST"
+				print(message, timestamp)
+				send_message(telegram_token, chat_id, message)
 			elif response_dict.get("status") == "found" and response_dict.get("request_query"):
 				timestamp = response_dict.get("last_attempt_timestamp")
-				print("GOT IT", timestamp)
+				message = "GOT IT"
+				print(message, timestamp)
+				send_message(telegram_token, chat_id, message)
 			else:
-				print("!!!!!!!!!!!!!!!!!!!!!!")
+				message = "!!!!!!!!!!!!!!!!!!!!!!"
+				print(message)
+				send_message(telegram_token, chat_id, message)
 		except requests.exceptions.ReadTimeout as error:
-			print("ERROR:", error)
+			message = f"ERROR: {error}"
+			print(message)
+			send_message(telegram_token, chat_id, message)
 			continue
 		except requests.exceptions.ConnectionError as error:
-			print("ERROR:", error)
-			time.sleep(60)
+			message = f"ERROR: {error}"
+			print(message)
+			send_message(telegram_token, chat_id, message)
+			time.sleep(1)
 			continue
 
 
