abb545bd052f33d1238b8229b8cdd8e88c3fff2a
jmuriki <106560016+jmuriki@users.noreply.github.com>
2023-01-25 11:34:39 +0530

    Add except with ReadTimeout & ConnectionError

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import json
+import time
 import requests
 
 from dotenv import load_dotenv
@@ -17,24 +18,32 @@ def main():
 		params = {
 			"timestamp": timestamp,
 		}
-		response = requests.get(long_polling_url, headers=header, params=params)
-		response.raise_for_status()
-		print(response.text)
-		response_dict = json.loads(response.text)
-		if response_dict.get("status") == "timeout" and not response_dict.get("request_query"):
-			timestamp = response_dict.get("timestamp_to_request")
-			print("FIRST WAITING", timestamp)
-		elif response_dict.get("status") == "timeout" and response_dict.get("request_query"):
-			timestamp = response_dict.get("timestamp_to_request")
-			print("AGAIN WAITING", timestamp)
-		elif response_dict.get("status") == "found" and not response_dict.get("request_query"):
-			timestamp = response_dict.get("last_attempt_timestamp")
-			print("GOT IT FAST", timestamp)
-		elif response_dict.get("status") == "found" and response_dict.get("request_query"):
-			timestamp = response_dict.get("last_attempt_timestamp")
-			print("GOT IT", timestamp)
-		else:
-			print("!!!!!!!!!!!!!!!!!!!!!!")
+		try:
+			response = requests.get(long_polling_url, headers=header, params=params)
+			response.raise_for_status()
+			print(response.text)
+			response_dict = json.loads(response.text)
+			if response_dict.get("status") == "timeout" and not response_dict.get("request_query"):
+				timestamp = response_dict.get("timestamp_to_request")
+				print("WAITING FOR THE FIRST TIME", timestamp)
+			elif response_dict.get("status") == "timeout" and response_dict.get("request_query"):
+				timestamp = response_dict.get("timestamp_to_request")
+				print("WAITING", timestamp)
+			elif response_dict.get("status") == "found" and not response_dict.get("request_query"):
+				timestamp = response_dict.get("last_attempt_timestamp")
+				print("GOT IT FAST", timestamp)
+			elif response_dict.get("status") == "found" and response_dict.get("request_query"):
+				timestamp = response_dict.get("last_attempt_timestamp")
+				print("GOT IT", timestamp)
+			else:
+				print("!!!!!!!!!!!!!!!!!!!!!!")
+		except requests.exceptions.ReadTimeout as error:
+			print("ERROR:", error)
+			continue
+		except requests.exceptions.ConnectionError as error:
+			print("ERROR:", error)
+			time.sleep(60)
+			continue
 
 
 if __name__ == "__main__":
